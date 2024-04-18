d5cfa8b646001a00a0958af2c717c3fd9ba86362
Cinex10 <y.driss@esi-sba.dz>
2024-04-18 07:22:53 +0100

    Add data,model and train script

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ def main():
 
     wandb_logger = WandbLogger(project="CycleGAN",log_model=True)
     checkpoint_callback = ModelCheckpoint(dirpath=f'checkpoints/{expirement_name}',monitor='train/gen_loss',filename='{epoch}-{train/gen_loss:.2f}')
-    trainer = L.Trainer(accelerator='auto',max_epochs=3,logger=wandb_logger,callbacks=[checkpoint_callback])
+    trainer = L.Trainer(accelerator='auto',max_epochs=3,logger=wandb_logger,callbacks=[checkpoint_callback],strategy='ddp_find_unused_parameters_true')
 
     trainer.fit(model, data)
 
