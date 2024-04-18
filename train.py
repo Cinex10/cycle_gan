ed050459d828075332e09bd6f00248e409159f0a
Cinex10 <y.driss@esi-sba.dz>
2024-04-18 06:09:22 +0100

    Add data,model and train script

--- train.py
+++ train.py
@@ -8,17 +8,20 @@ import lightning as L
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.callbacks import ModelCheckpoint
 
-expirement_name = 'example'
+def main():
+    expirement_name = 'example'
 
+    data = UnpairedDataset('datasets/edges2shoes', 'train')
+    data = DataLoader(data, batch_size=1, shuffle=True)
+    model = CycleGan()
 
-data = UnpairedDataset('datasets/edges2shoes', 'train')
-data = DataLoader(data, batch_size=1, shuffle=True)
-model = CycleGan()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    wandb_logger = WandbLogger(project="CycleGAN")
+    checkpoint_callback = ModelCheckpoint(dirpath=f'checkpoints/{expirement_name}',monitor='train/gen_loss',filename='{epoch}-{train/gen_loss:.2f}')
+    trainer = L.Trainer(accelerator='auto',max_epochs=3,logger=wandb_logger,callbacks=[checkpoint_callback])
 
-wandb_logger = WandbLogger(project="CycleGAN")
-checkpoint_callback = ModelCheckpoint(dirpath=f'checkpoints/{expirement_name}',monitor='train/gen_loss',filename='{epoch}-{train/gen_loss:.2f}')
-trainer = L.Trainer(accelerator='auto',max_epochs=3,logger=wandb_logger)
+    trainer.fit(model, data)
 
-trainer.fit(model, data)
+if __name__ == '__main__':
+    main()
